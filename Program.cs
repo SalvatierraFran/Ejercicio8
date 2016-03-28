029a4ef6a0664ca52bb420e8b1c48514f8deec9b
SalvatierraFran <franco14_93@hotmail.com>
2016-03-28 00:00:37 -0300

    Se le agregó la opción de que el usuario pueda elegir si continuar o no hasta 'n' cantidad de empleados.

--- Program.cs
+++ Program.cs
@@ -11,30 +11,43 @@ namespace Ejercicio8
         {
             Console.Title = "Ejercicio 08";
 
+            char rta = 's';
 
-            Console.Write("Ingrese valor hora: ");
-            double Valhora = double.Parse(Console.ReadLine());
+            do
+            {
+                Console.Write("Ingrese valor hora: ");
+                double Valhora = double.Parse(Console.ReadLine());
 
-            Console.Write("Ingrese nombre: ");
-            string Nombre = Console.ReadLine();
+                Console.Write("Ingrese nombre: ");
+                string Nombre = Console.ReadLine();
 
-            Console.Write("Ingrese antigüedad: ");
-            double Antiguedad = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese antigüedad: ");
+                double Antiguedad = double.Parse(Console.ReadLine());
 
-            Console.Write("Ingrese cantidad de horas trabajadas: ");
-            double Canthoras = double.Parse(Console.ReadLine());
+                Console.Write("Ingrese cantidad de horas trabajadas: ");
+                double Canthoras = double.Parse(Console.ReadLine());
 
-            double TotalBruto = (Canthoras * Valhora) + (Antiguedad * 150);
+                double TotalBruto = (Canthoras * Valhora) + (Antiguedad * 150);
 
-            double Descuentos = (TotalBruto * 13) / 100;
+                double Descuentos = (TotalBruto * 13) / 100;
 
-            double TotalNeto = TotalBruto - Descuentos;
+                double TotalNeto = TotalBruto - Descuentos;
 
-            Console.WriteLine("");
+                Console.WriteLine("");
 
-            Console.Write("Nombre\tAntigüedad\tValorHora\tSalarioBruto\tDescuentos\tSalarioNeto\n");
-            Console.Write("{0}\t{1}\t\t{2:f2}\t\t{3:f2}\t\t{4:f2}\t\t{5:f2}", Nombre, Antiguedad, Valhora, TotalBruto, Descuentos, TotalNeto);
+                Console.Write("Nombre\tAntigüedad\tValorHora\tSalarioBruto\tDescuentos\tSalarioNeto\n");
+                Console.Write("{0}\t{1}\t\t{2:f2}\t\t{3:f2}\t\t{4:f2}\t\t{5:f2}", Nombre, Antiguedad, Valhora, TotalBruto, Descuentos, TotalNeto);
 
+                Console.Write("\n\nDesea continuar ingresando? S/N: ");
+                rta = char.Parse(Console.ReadLine());
+
+                if (rta != 's' && rta != 'n' && rta != 'S' && rta != 'N')
+                {
+                    Console.Write("\nError. Reingrese: ");
+                    rta = char.Parse(Console.ReadLine());
+                }
+
+            } while (rta == 's');
             Console.ReadKey();
         }
     }
