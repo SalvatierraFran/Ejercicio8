48ef4bd788ec857b117f0ee1fc2b0a89d9dbbce7
SalvatierraFran <franco14_93@hotmail.com>
2016-03-27 22:43:19 -0300

    Programa que calcula el total en bruto y el total neto a cobrar de un empleado.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 08";
+
+
+            Console.Write("Ingrese valor hora: ");
+            double Valhora = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese nombre: ");
+            string Nombre = Console.ReadLine();
+
+            Console.Write("Ingrese antigüedad: ");
+            double Antiguedad = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese cantidad de horas trabajadas: ");
+            double Canthoras = double.Parse(Console.ReadLine());
+
+            double TotalBruto = (Canthoras * Valhora) + (Antiguedad * 150);
+
+            double Descuentos = (TotalBruto * 13) / 100;
+
+            double TotalNeto = TotalBruto - Descuentos;
+
+            Console.WriteLine("");
+
+            Console.Write("Nombre: " + Nombre);
+            Console.Write("\nAntigüedad: " + Antiguedad);
+            Console.Write("\nValor de hora: " + Valhora);
+            Console.Write("\nCobrar en bruto: " + TotalBruto);
+            Console.Write("\nDescuentos: " + Descuentos);
+            Console.Write("\nValor neto a cobrar: " + TotalNeto);
+
+            Console.ReadKey();
+        }
+    }
+}
